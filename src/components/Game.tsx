3cb2855a98842bb02b8e02d639f71db7af03524e
Knightingal <yujianming1985@hotmail.com>
2018-06-01 23:15:24 +0800

    add ball

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,70 +1,102 @@
 import * as React from 'react';
+interface BallInfo {
+    id: number;
+    gridX: number;
+    gridY: number;
+}
+class BallComponent extends React.Component<{ballInfo: BallInfo},{}> {
+    static margin: number = 3;
+    constructor(props: {ballInfo: BallInfo}) {
+        super(props);
+    }
 
-export class Game extends React.Component<{}, {}> {
-    OFFSET_TOP=3;
-    OFFSET_LEFT=3;
-    GRID_SIZE=30;
-    GRID_COUNT=9;
+    render() {
+        return <rect 
+            className="rect-ball"
+            x={Game.OFFSET_TOP + this.props.ballInfo.gridX * Game.GRID_SIZE + BallComponent.margin} 
+            y={Game.OFFSET_TOP + this.props.ballInfo.gridY * Game.GRID_SIZE + BallComponent.margin} 
+            width={Game.GRID_SIZE - BallComponent.margin * 2}
+            height={Game.GRID_SIZE - BallComponent.margin * 2}
+        />
+    }
 
-    createHLine(index:number) {
+}
+
+export class Game extends React.Component<{}, {balls: Array<BallInfo>}> {
+    static OFFSET_TOP = 3;
+    static OFFSET_LEFT = 3;
+    static GRID_SIZE = 30;
+    static GRID_COUNT = 9;
+    static ballIdSeq = 1;
+
+    createHLine(index: number) {
         return <line 
-            className="game-line" key={index}
-            x1={this.OFFSET_LEFT} 
-            y1={this.GRID_SIZE*index + this.OFFSET_TOP} 
-            x2={this.GRID_SIZE*this.GRID_COUNT+this.OFFSET_LEFT} 
-            y2={this.GRID_SIZE*index + this.OFFSET_TOP}/>
+            className="game-line" 
+            key={index}
+            x1={Game.OFFSET_LEFT} 
+            y1={Game.GRID_SIZE * index + Game.OFFSET_TOP} 
+            x2={Game.GRID_SIZE * Game.GRID_COUNT + Game.OFFSET_LEFT} 
+            y2={Game.GRID_SIZE * index + Game.OFFSET_TOP}/>
     }
 
     createVLine(index:number) {
         return <line 
-            className="game-line" key={index}
-            y1={this.OFFSET_TOP} 
-            x1={this.GRID_SIZE*index + this.OFFSET_LEFT} 
-            y2={this.GRID_SIZE*this.GRID_COUNT+this.OFFSET_TOP} 
-            x2={this.GRID_SIZE*index + this.OFFSET_LEFT}/>
+            className="game-line" 
+            key={index}
+            y1={Game.OFFSET_TOP} 
+            x1={Game.GRID_SIZE * index + Game.OFFSET_LEFT} 
+            y2={Game.GRID_SIZE * Game.GRID_COUNT + Game.OFFSET_TOP} 
+            x2={Game.GRID_SIZE * index + Game.OFFSET_LEFT}/>
     }
 
 
 
     createHLines() {
-        const ret=new Array<React.ReactNode>();
-        for (let i:number=0;i<this.GRID_COUNT+1;i++) {
+        const ret = new Array<React.ReactNode>();
+        for (let i: number = 0; i < Game.GRID_COUNT + 1; i++) {
             ret.push(this.createHLine(i));
         }
         return ret;
     }
 
     createVLines() {
-        const ret=new Array<React.ReactNode>();
-        for (let i:number=0;i<this.GRID_COUNT+1;i++) {
+        const ret = new Array<React.ReactNode>();
+        for (let i: number = 0; i < Game.GRID_COUNT + 1; i++) {
             ret.push(this.createVLine(i));
         }
         return ret;
     }
 
-    constructor(props:{}) {
+    constructor(props: {}) {
         super(props);
+        this.state = {balls: new Array<BallInfo>()};
     }
 
-    handleClick(e:React.MouseEvent<HTMLDivElement>) {
+    handleClick(e: React.MouseEvent<HTMLDivElement>) {
         const x = e.nativeEvent.offsetX;
         const y = e.nativeEvent.offsetY;
-        if (x < this.OFFSET_LEFT || y < this.OFFSET_TOP || x > this.GRID_COUNT * this.GRID_SIZE || y > this.GRID_COUNT * this.GRID_SIZE) {
+        if (x < Game.OFFSET_LEFT || y < Game.OFFSET_TOP || x > Game.GRID_COUNT * Game.GRID_SIZE || y > Game.GRID_COUNT * Game.GRID_SIZE) {
             return;
         }
-        const gridX = Math.floor((x - this.OFFSET_LEFT) / this.GRID_SIZE);
-        const gridY = Math.floor((y - this.OFFSET_LEFT) / this.GRID_SIZE);
+        const gridX = Math.floor((x - Game.OFFSET_LEFT) / Game.GRID_SIZE);
+        const gridY = Math.floor((y - Game.OFFSET_LEFT) / Game.GRID_SIZE);
         console.log(`touch column ${gridX}, line ${gridY}`);
-
+        this.setState({
+            balls:this.state.balls.concat({id: Game.ballIdSeq++, gridX: gridX, gridY: gridY})
+        });
     }
 
 
     render() {
+        let ballComponents = this.state.balls.map((ball)=>{
+            return <BallComponent key={ball.id} ballInfo={ball} />;
+        });
         return (
             <div>
             <svg className="game-svg" width="1000px" height="1000px" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 {this.createHLines()}        
                 {this.createVLines()}        
+                {ballComponents}
             </svg>
             <div className="touch-layer" onClick={(e)=>this.handleClick(e)}></div>
             </div>
